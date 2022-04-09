4facadcae3ac2b36fde3c6335811de52e2a97612
Oscar Guajardo <45528951+oscarguajardo167@users.noreply.github.com>
2022-04-09 18:36:49 -0500

    Add Lista de Pacientes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Formulario() {
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 ">
 
       <h2 className="font-black text-3xl text-center ">Seguimiento Pacientes</h2>
 
--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Paciente from './Paciente'
+
 
 function ListadoPacientes() {
   return (
-    <div className="md:w-1/2 lg:w-3/5">
+    <div className="md:w-1/2 lg:w-3/5  ">
         <h2 className="font-black text-3xl text-center">Listado de Pacientes</h2>
+        
+        <p className=" text-lg mt-5 text-center mb-10">
+        Administra tus {''}
+        <span className="text-sky-600  font-bold"> Pacientes y Citas</span>
+        </p>
+        <div className='md:overflow-y-scroll h-screen'>
+          <Paciente/>
+          <Paciente/>
+          <Paciente/>
+        </div>
+        
+               
 
     </div>
   )
